7128d5f9842931df9be88a39a73a8bb996fb6306
Spartak3 <spartak.vardanyan3@yahoo.com>
2021-03-27 02:22:01 +0400

    new commit

--- src/Home.java
+++ src/Home.java
@@ -1,5 +1,19 @@
+import javax.sound.midi.Soundbank;
+
 public class Home {
     public static void main(String[] args) {
-        System.out.println("This is America");
+        byte by=5;
+        short shor=25;
+        int num=33;
+        long lnum=2345L;
+        float fnum=34.45f;
+        double dnum=32.32;
+        boolean bval=true;
+        char symb='s';
+        String str="Java";
+        System.out.println("Byte value: "+by+", Short value: "+shor+", Int value: "+num+", Long value: "+lnum+
+                ", Float value: "+fnum+", Double value: "+dnum+", Boolean value: "+bval+", Char value: "+symb+" String value: "+str+" :");
+        System.out.print("Thanks)");
+
     }
 }
