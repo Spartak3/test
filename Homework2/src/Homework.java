e1d41fded80b93d8fd07ff38cf47bf45f73c61c4
Spartak3 <spartak.vardanyan3@yahoo.com>
2021-04-04 00:59:53 +0400

    h3 commit

---
+++ Homework2/src/Homework.java
@@ -0,0 +1,82 @@
+import java.io.Console;
+import java.util.Scanner;
+
+public class Homework {
+    public static void main(String[] args) {
+       //task1-Create an array and fill it with 2 number.
+        int[] arr1=new int[]{2,4};
+
+        //task2-Create an array and fill it with numbers from 1:1000.
+        int size=1000;
+        int[] arr2=new int[size];
+        for (int i=0;i<size;i++){
+            arr2[i]=i+1;
+        }
+
+
+        //task3-Create an array and fill it with odd numbers from -20:20
+        int size3=20,curr=0;
+        int[] arr3=new int[size3];
+        for (int i=-20;i<=20;i++)
+        {
+            if(i%2!=0) {
+                arr3[curr++] = i;
+            }
+        }
+
+        //task4-Create an array and fill it. Print all elements which can be divided by 5
+        int[] arr4={1,2,4,443,32,45,68,77,25,35,60};
+        for (int i=0;i<arr4.length;i++)
+        {
+            if(arr4[i]%5==0)
+                System.out.print(arr4[i]+" ");
+        }
+        System.out.println();
+
+
+        //task5-Create an array and fill it. Print all elements which are between 24.12 and 467.23.
+        float[] arr5={1.0f,.45f,4.456f,443.5f,32f,45f,68.54f,77.0f,25.25f,35.3f,60.0f};
+        for (int i=0;i<arr5.length;i++)
+        {
+            if(arr5[i]>=24.12 && arr5[i]<=467.23){
+                System.out.print(arr5[i]+" ");
+            }
+        }
+        System.out.println();
+
+        //task6-Create an array and fill it. Print count of elements which can be divided by 2.
+        int[] arr6={1,2,4,443,32,45,68,77,25,35,60};
+        int sumOf=0;
+        for (int i=0;i<arr4.length;i++)
+        {
+            if(arr6[i]%2==0)
+                sumOf++;
+
+        }
+        System.out.println(sumOf);
+
+
+
+        //task7-Create an array and fill it. Print count of elements which can be divided by 2.
+        int num= 8;
+        for (int i=1;i<=10;i++)
+        {
+            System.out.println(num+" x "+i+" = "+num*i);
+        }
+
+        //Find single element
+        int[] arrOfNum={1,2,3,2,7,1,3,8,8};//7 is single
+        int singleNum=arrOfNum[0];
+        for (int i = 1; i <arrOfNum.length; i++) {
+            singleNum=singleNum^arrOfNum[i];
+        }
+        System.out.printf("Single element is %d",singleNum);
+
+
+
+
+
+
+
+    }
+}
--- Operations/src/Operator.java
+++ Operations/src/Operator.java
@@ -1,5 +1,40 @@
+import java.awt.font.FontRenderContext;
+
 public class Operator {
+
+
+    static int isPowerOfFour(int n)
+    {
+        int count = 0;
+
+        /*Check if there is
+        only one bit set in n*/
+        int x = n & (n - 1);
+
+        if ( n > 0 && x == 0)
+        {
+            /* count 0 bits
+            before set bit */
+            while(n > 1)
+            {
+                n >>= 1;
+                count += 1;
+            }
+
+            /*If count is even
+            then return true
+            else false*/
+            return (count % 2 == 0) ? 1 : 0;
+        }
+
+            /* If there are more than
+            1 bit set then n is not a
+            power of 4*/
+        return 0;
+    }
+
     public static void main(String[] args) {
+        /*
         int a=5,b=10;
         int c=a+b,g=a*b;
         System.out.println("c="+c+", g="+g);
@@ -28,6 +63,62 @@ public class Operator {
         System.out.println("arr[0]="+arr[0]+", arr[1]="+arr[1]+", arr[2]="+arr[2]);
         arr[0]=arr[1]+arr[2];
         System.out.println("arr[0] is now "+arr[0]);
+         */
+
+        /*
+        int[] arr = {1, 2, 5, 4, 6, 8, 9, 2, 1, 4, 5, 9,6};
+        int v = 0;
+        for (int i=0;i<arr.length;i++){
+            v = v ^ arr[i];
+            //System.out.println("i= "+i+", "+v);
+        }
+        //System.out.println(v);
+
+        int num=122111474;
+        int k=0,t=0,n=num;
+        while(n!=0)
+        {
+            k=n%10;
+            t=t^k;
+            n/=10;
+        }*/
+        /*
+        int num1=30,num2=10;
+        num1=num1^num2;
+        num2=num1^num2;
+        num1=num1^num2;
+        System.out.println(num1+" "+num2);
+        System.out.println(isPowerOfFour(64));
+         */
+
+
+        int n=10;
+        int[] A=new int[]{1,2,3,4,5,34,7,8,9,10};
+        int min=A[0];
+        for (int i=0;i<n;i++)
+        {
+            if(min>A[i])
+                min=A[i];
+        }
+        System.out.println(min);
+        if(n>10)
+            System.out.println("sss");
+        else if(n<10)
+        {
+            System.out.println("ddd");
+        }
+        System.out.println("----------------");
+
+        int k=5;
+        for (int i=0;i<=k;i++)
+        {
+            for (int j=0;j<i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
 
 
 
